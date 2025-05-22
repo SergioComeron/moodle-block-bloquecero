fe40212b460fbe4ad513bfbac1e8856f6146a6dd
SergioComeron <sergiocomeron@icloud.com>
2025-05-22 17:32:04 +0200

    wip. elimino cadenas string

--- lang/en/block_bloquecero.php
+++ lang/en/block_bloquecero.php
@@ -1,15 +1,5 @@
 <?php
 $string['pluginname'] = 'Bloque cero';
-$string['clientid'] = 'Client ID';
-$string['clientid_desc'] = 'Introduce el Client ID de OAuth2';
-$string['clientsecret'] = 'Client Secret';
-$string['clientsecret_desc'] = 'Introduce el Client Secret de OAuth2';
-$string['tokenurl'] = 'Token URL';
-$string['tokenurl_desc'] = 'Introduce la URL de autenticación OAuth2';
-$string['scope'] = 'Scope';
-$string['scope_desc'] = 'Introduce el scope de la API';
-$string['errorfetching'] = 'Error obteniendo los datos';
-$string['invalidresponse'] = 'Respuesta inválida de la API';
 $string['calificador'] = 'Grader';
 $string['tareas'] = 'Tasks';
 $string['participantes'] = 'Participants';
