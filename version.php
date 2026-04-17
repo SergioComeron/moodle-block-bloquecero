0302ce9b319744a9a5750ab5c7c2baeebc1ce820
SergioComeron <sergiocomeron@icloud.com>
2026-04-17 16:38:21 +0200

    Release: bump version to 0.10

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_bloquecero';
-$plugin->version = 2026041700;
+$plugin->version = 2026041701;
 $plugin->requires = 2021051700; // Moodle 3.11 o superior.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '0.9';
+$plugin->release = '0.10';
